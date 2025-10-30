c33d91013226a1df06a5d566300fe58b5889b1a7
Chukeze <enile12@gmail.com>
2025-10-29 19:20:48 -0500

    Fix layout export

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -10,7 +10,7 @@ const RESOURCES = {
  // es: { common: esCommon, dashboard: esDashboard },
 }
 
-export function RootLayout({
+export default function RootLayout({
   children,
   params: { locale },
 }: {
